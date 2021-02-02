42b6b31462c874bd149fd5e2ce07d53888ff97c1
moazbasheer <moazbasheer97@gmail.com>
2021-02-02 11:37:04 +0200

    add task 1 and 2

--- proj1.sql
+++ proj1.sql
@@ -19,49 +19,75 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE weight > 300
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE namefirst LIKE '% %'
+  ORDER BY namefirst, namelast
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), count(*)
+  FROM people
+  GROUP BY birthyear
+  ORDER BY birthyear ASC
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), count(*)
+  FROM people
+  GROUP BY birthyear
+  HAVING AVG(height) > 70
+  ORDER BY birthyear ASC
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, p.playerID, yearid
+  FROM people as p, halloffame as h
+  WHERE p.playerID = h.playerID AND h.inducted = 'Y'
+  order by yearid desc, p.playerID asc;
 ;
 
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, p.playerid, s.schoolid, h.yearid
+  FROM people as p, halloffame as h, collegeplaying as cp, schools as s
+  WHERE p.playerid = h.playerid AND h.inducted = 'Y' AND p.playerid = cp.playerid
+  AND cp.schoolID = s.schoolID AND s.schoolState = 'CA'
+  order by h.yearid desc, s.schoolid asc, p.playerid asc
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, namefirst, namelast, cp.schoolid
+  FROM (people as p LEFT OUTER JOIN collegeplaying as cp
+	ON cp.playerid = p.playerid)
+  INNER JOIN halloffame as h ON p.playerid = h.playerid
+  WHERE h.inducted = 'Y'
+  order by p.playerid desc, cp.schoolid asc
+
 ;
 
 -- Question 3i
